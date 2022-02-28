fc86b38bf6c83e5ef96718b68356d26a79ee577c
rounak <rounakkole@gmail.com>
2022-02-28 21:57:46 +0530

    [rounak] ADDED: find frequency of words in a sentence

--- Program.cs
+++ Program.cs
@@ -8,6 +8,24 @@ namespace HashTable
         static void Main(string[] args)
         {
             Console.WriteLine("Hash Table");
+            String[] Sentence = { "TO", "BE", "OR", "NOT", "TO", "BE" };
+            WordFrequency(Sentence);
+        }
+
+
+        public static void WordFrequency(string[] Sentence)
+        {
+            MyMapNode<string, int> MyMapNode = new MyMapNode<string, int>(5);
+            foreach (string word in Sentence)
+            {
+                int count = MyMapNode.CountFrequency(word);
+                MyMapNode.Add(word, count);
+            }
+            IEnumerable<string> DistinctSentence = Sentence.Distinct<string>();
+            foreach (string word in DistinctSentence)
+            {
+                MyMapNode.Display(word);
+            }
         }
     }
 }
